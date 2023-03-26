f82ae9d549b0c3690527b4fc5ec926d112a8eb21
Eugene Kosukhin <kosuha606@gmail.com>
2023-03-26 11:41:27 +0300

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,18 @@
 
 <template>
   <main>
-    Главная страница
+    <v-container>
+      <v-btn
+        @click="$router.push('/task/new')"
+        class="mb-4"
+        color="indigo-darken-3"
+      >Добавить задачу</v-btn>
+      <v-card>
+        <v-card-title>Нет задач</v-card-title>
+        <v-card-text>
+          Чтобы добавить задачу нажмите "Добавить задачу"
+        </v-card-text>
+      </v-card>
+    </v-container>
   </main>
 </template>
---
+++ src/views/TaskView.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <v-container>
+    Новая задача
+  </v-container>
+</template>
