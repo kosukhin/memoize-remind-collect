78787d953cd732a92e93526623162e808fec10ac
Eugene Kosukhin <kosuha606@gmail.com>
2023-03-26 16:44:46 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,46 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/ms-icon-310x310.png",
+            sizes: "310x310",
+            type: "image/png",
+          },
+          {
+            src: "ms-icon-310x310.png",
+            sizes: "310x310",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
